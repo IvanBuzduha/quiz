a6bb1c11e4c78365838e8b7fbae4f2e56ea5f60c
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-02-04 20:15:25 +0200

    add main files

---
+++ src/components/Game.jsx
@@ -0,0 +1,21 @@
+import "../index.scss";
+import questions from "../quesrtions.json";
+export const Game = ({ question, onClickVariant, step }) => {
+  const percentage = Math.round((step / questions.length) * 100);
+//   console.log("percentage", percentage);
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner" />
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
